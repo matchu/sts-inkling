5bbac6d28f3267bb472bd918cd76c4fd4be13f82
Matchu <matchu1993@gmail.com>
2022-09-16 06:46:46 -0700

    Make Inkling a more normal class

--- theDefault/src/main/java/inklingMod/characters/TheInkling.java
+++ theDefault/src/main/java/inklingMod/characters/TheInkling.java
@@ -68,11 +68,11 @@ public class TheInkling extends CustomPlayer {
     // =============== BASE STATS =================
 
     public static final int ENERGY_PER_TURN = 3;
-    public static final int STARTING_HP = 75;
-    public static final int MAX_HP = 75;
+    public static final int STARTING_HP = 70;
+    public static final int MAX_HP = 70;
     public static final int STARTING_GOLD = 99;
-    public static final int CARD_DRAW = 9;
-    public static final int ORB_SLOTS = 3;
+    public static final int CARD_DRAW = 5;
+    public static final int ORB_SLOTS = 0;
 
     // =============== /BASE STATS/ =================
 
