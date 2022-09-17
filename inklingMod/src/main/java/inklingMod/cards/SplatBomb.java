93ca04bf1977119a63cbcd57a4c95a2c3de1d102
Matchu <matchu1993@gmail.com>
2022-09-16 23:07:00 -0700

    Add Splat Bomb card, with Poison instead of Ink
    The card text says Ink, but Ink isn't implemented yet, so it applies Poison instead just to check our understanding of the systems!

---
+++ inklingMod/src/main/java/inklingMod/cards/SplatBomb.java
@@ -0,0 +1,61 @@
+package inklingMod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+
+import basemod.abstracts.CustomCard;
+import inklingMod.InklingMod;
+import inklingMod.characters.TheInkling;
+
+public class SplatBomb extends CustomCard {
+  public static final String ID = InklingMod.makeID(SplatBomb.class.getSimpleName());
+  private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+  public static final String NAME = cardStrings.NAME;
+  public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+  public static final String IMG = InklingMod.makeCardPath("SplatBomb.png");
+  private static final CardRarity RARITY = CardRarity.BASIC;
+  private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+  private static final CardType TYPE = CardType.SKILL;
+  public static final CardColor COLOR = TheInkling.Enums.COLOR_GRAY;
+
+  private static final int COST = 1;
+  private static final int INK_AMOUNT = 6;
+  private static final int UPGRADE_ADDITIONAL_INK_AMOUNT = 4;
+
+  public SplatBomb() {
+    super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+
+    this.baseMagicNumber = INK_AMOUNT;
+    this.magicNumber = this.baseMagicNumber;
+  }
+
+  @Override
+  public void use(AbstractPlayer p, AbstractMonster m) {
+    // Copied from Cripping Cloud, idk why they're so particular about non-dead tho.
+    // Could just be to speed up the UI in certain cases where cards are
+    // auto-played?
+    if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+      for (AbstractMonster monster : AbstractDungeon.getMonsters().monsters) {
+        if (!monster.isDead && !monster.isDying) {
+          addToBot(new ApplyPowerAction(
+              monster, p, new PoisonPower(monster, p, this.magicNumber), this.magicNumber));
+        }
+      }
+    }
+  }
+
+  @Override
+  public void upgrade() {
+    if (!upgraded) {
+      upgradeName();
+      upgradeMagicNumber(UPGRADE_ADDITIONAL_INK_AMOUNT);
+      initializeDescription();
+    }
+  }
+}
---
+++ inklingMod/src/main/java/inklingMod/powers/InkPower.java
@@ -0,0 +1,33 @@
+// package inklingMod.powers;
+
+// import com.badlogic.gdx.graphics.Texture;
+// import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+// import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+// import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+// import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+// import com.megacrit.cardcrawl.cards.AbstractCard;
+// import com.megacrit.cardcrawl.characters.AbstractPlayer;
+// import com.megacrit.cardcrawl.core.AbstractCreature;
+// import com.megacrit.cardcrawl.core.CardCrawlGame;
+// import com.megacrit.cardcrawl.localization.PowerStrings;
+// import com.megacrit.cardcrawl.powers.AbstractPower;
+
+// import basemod.interfaces.CloneablePowerInterface;
+// import inklingMod.InklingMod;
+// import inklingMod.util.TextureLoader;
+
+// public class InkPower extends AbstractPower implements
+// CloneablePowerInterface {
+// public static final String POWER_ID =
+// InklingMod.makeID(InkPower.class.getSimpleName());
+// private static final PowerStrings powerStrings =
+// CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+// public static final String NAME = powerStrings.NAME;
+// public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+// private static final Texture tex84 = TextureLoader.getTexture(
+// InklingMod.makePowerPath("ink_power84.png"));
+// private static final Texture tex32 = TextureLoader.getTexture(
+// InklingMod.makePowerPath("ink_power32.png"));
+
+// }
