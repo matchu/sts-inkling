3531d50b6960d8388cbc1848dda5105c851e691e
Matchu <matchu1993@gmail.com>
2022-09-18 00:45:53 -0700

    Oops, fix Fizzy Bomb random targeting!
    Right, I misread my own card text lol. The reason it was safe to go quadratic was that this card text *can't* just apply to one monster quadratically; it HAS to be spread across swarms. That's much safer imo!

--- inklingMod/src/main/java/inklingMod/actions/FizzyBombAction.java
+++ inklingMod/src/main/java/inklingMod/actions/FizzyBombAction.java
@@ -1,5 +1,8 @@
 package inklingMod.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -8,6 +11,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.random.Random;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public class FizzyBombAction extends AbstractGameAction {
@@ -38,17 +42,18 @@ public class FizzyBombAction extends AbstractGameAction {
       effect += 2;
       this.player.getRelic("Chemical X").flash();
     }
-    if (effect > 0) {
-      for (int i = 0; i < effect; i++) {
-        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
-          AbstractMonster randomMonster = AbstractDungeon.getMonsters().getRandomMonster(
-              null, true, AbstractDungeon.cardRandomRng);
-          addToBot(new DamageAction(randomMonster,
+    if (effect > 0 && !AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+      // Note that we're repeating for N enemies total, NOT just N times!
+      // This prevents us from being a cheap Killer Wail lol; it's crowd
+      // control specifically!
+      List<AbstractMonster> targets = getRandomMonsterTargets(
+          effect, AbstractDungeon.cardRandomRng);
+      for (AbstractMonster target : targets) {
+        for (int i = 0; i < effect; i++) {
+          addToBot(new DamageAction(target,
               new DamageInfo(this.player, this.damage, this.damageType),
-              AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-          addToBot(new DamageAction(randomMonster,
-              new DamageInfo(this.player, this.damage, this.damageType),
-              AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+              i % 2 == 0 ? AbstractGameAction.AttackEffect.SLASH_HORIZONTAL
+                  : AbstractGameAction.AttackEffect.SLASH_VERTICAL));
         }
       }
       if (!this.freeToPlayOnce) {
@@ -56,5 +61,35 @@ public class FizzyBombAction extends AbstractGameAction {
       }
     }
     this.isDone = true;
+  }
+
+  private List<AbstractMonster> getRandomMonsterTargets(int numToTarget, Random rng) {
+    // Find all monsters that are not "basically dead".
+    List<AbstractMonster> validTargets = new ArrayList<AbstractMonster>();
+    for (AbstractMonster monster : AbstractDungeon.getMonsters().monsters) {
+      if (!monster.isDying && !monster.isEscaping) {
+        validTargets.add(monster);
+      }
+    }
+
+    // If all of the monsters fit in `numToTarget`, just return them all.
+    if (validTargets.size() <= numToTarget) {
+      return validTargets;
+    }
+
+    // Otherwise, randomly choose `numToTarget` of them to target.
+    // We'll mutate `validTargets` to do this, which is fine, because we're not
+    // going to return it so no one else will ever see it!
+    List<AbstractMonster> targets = new ArrayList<AbstractMonster>();
+    for (int i = 0; i < numToTarget; i++) {
+      // Choose a random index in `validTargets`. Remove the monster at that
+      // index from `validTargets` and add it to `targets`.
+      int randomIndexOfValidTargets = rng.random(0, validTargets.size() - 1);
+      AbstractMonster targetMonster = validTargets.get(randomIndexOfValidTargets);
+      validTargets.remove(randomIndexOfValidTargets);
+      targets.add(targetMonster);
+    }
+
+    return targets;
   }
 }
