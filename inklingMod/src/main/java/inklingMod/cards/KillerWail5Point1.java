660c78be1ebab7db393469a469013aaa51167de9
Matchu <matchu1993@gmail.com>
2022-09-18 00:50:36 -0700

    Fix Killer Wail targeting to match card text
    Lol I guess I just misinterpreted this! Anyway I'm still not gonna increase the numbers because it's still too fuckin good on one thing at 6x6, but I'll give the upgrade a bit more juice to compensate.

--- inklingMod/src/main/java/inklingMod/cards/KillerWail5Point1.java
+++ inklingMod/src/main/java/inklingMod/cards/KillerWail5Point1.java
@@ -2,6 +2,7 @@ package inklingMod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -20,13 +21,13 @@ public class KillerWail5Point1 extends CustomCard {
 
   public static final String IMG = InklingMod.makeCardPath("KillerWail5Point1.png");
   private static final CardRarity RARITY = CardRarity.RARE;
-  private static final CardTarget TARGET = CardTarget.ENEMY;
+  private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
   private static final CardType TYPE = CardType.ATTACK;
   public static final CardColor COLOR = TheInkling.Enums.COLOR_GRAY;
 
   private static final int COST = 3;
   private static final int DAMAGE = 3;
-  private static final int UPGRADE_PLUS_DMG = 2;
+  private static final int UPGRADE_PLUS_DMG = 3;
 
   public KillerWail5Point1() {
     super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
@@ -37,17 +38,23 @@ public class KillerWail5Point1 extends CustomCard {
 
   @Override
   public void use(AbstractPlayer p, AbstractMonster m) {
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-    addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn),
+    addToBot(new DamageRandomEnemyAction(
+        new DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_VERTICAL));
   }
 
