360e66ef0a41931a1978fb760da21026754f5be3
Matchu <matchu1993@gmail.com>
2022-10-07 17:34:17 -0700

    Nerf Dapple Dualies a bit
    They're delightfully flexible and I think they just don't need the base damage to be good. And I'd like the upgrade to look more impressive, but I don't actually want it to be higher, lol :p

--- inklingMod/src/main/java/inklingMod/cards/weapons/shots/DappleShot.java
+++ inklingMod/src/main/java/inklingMod/cards/weapons/shots/DappleShot.java
@@ -24,8 +24,8 @@ public class DappleShot extends CustomCard {
   public static final CardColor COLOR = CardColor.COLORLESS;
 
   private static final int COST = 1;
-  private static final int DAMAGE = 5;
-  private static final int UPGRADE_PLUS_DMG = 1;
+  private static final int DAMAGE = 4;
+  private static final int UPGRADE_PLUS_DMG = 2;
 
   public DappleShot() {
     super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
